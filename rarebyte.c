eadb2def04889bb123dd2b3db77923ca47430683
James Tirta Halim <tirtajames45@gmail.com>
2023-11-25 22:20:25 +0700

    corrected copyright notice

--- rarebyte.c
+++ rarebyte.c
@@ -1,5 +1,5 @@
 /* Copyright (c) 2023 James Tirta Halim <tirtajames45 at gmail dot com>
-   This file is part of the jstring library.
+   This file is part of rarebyte.
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
